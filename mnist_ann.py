71ec6dc4b5542525dae8753220a72180a5e06c5d
Colin Lu <1colin.lu@gmail.com>
2018-09-21 22:49:52 -0700

    revised comments

--- mnist_ann.py
+++ mnist_ann.py
@@ -4,22 +4,31 @@ from keras.datasets import mnist
 from keras.utils import np_utils
 import matplotlib.pyplot as plt
 import numpy as np
+
+# suppress warning/error messages in terminal
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-# reshape and convert to one-hot
+# load in data from mnist dataset (60k training, 10k test)
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# reshape and convert to one-hot
 x_train = x_train.reshape(x_train.shape[0], 784)
 x_test = x_test.reshape(x_test.shape[0], 784)
 y_train = np_utils.to_categorical(y_train, 10)
 y_test = np_utils.to_categorical(y_test, 10)
 
-# split off some data for validation (6k out of 60k)
+# split main training set into train/validation sets (6k out of 60k data points reserved for validation)
 x_validate = x_train[:6000, :]
 y_validate = y_train[:6000, :]
 x_train = x_train[6000:, :]
 y_train = y_train[6000:, :]
 
+'''
+Building layers for the feedforward NN:
+Hidden layers have 56, 96, 96, and 56 nodes, in that order.
+Takes in 784 values (pixel input) and outputs 10 values (predicted probability for each number, 0-9).
+'''
 model = models.Sequential()
 model.add(layers.Dense(56,activation='relu',input_shape=(784,)))
 model.add(layers.Dense(96,activation='relu'))
@@ -31,10 +40,12 @@ model.compile(loss='categorical_crossentropy',
 			  optimizer='adam',
 			  metrics=['accuracy'])
 
+# training the NN
 epochs = 20
 history = model.fit(x_train,y_train,epochs=epochs,batch_size=512,verbose=2,validation_data=(x_validate,y_validate))
 print("Finished fitting.")
 
+# plotting learning curves during training (on both training and validation data)
 epoch_labels = range(1, epochs+1)
 hist_dict = history.history
 plt.title("Accuracy vs Epochs")
@@ -43,7 +54,8 @@ plt.plot(epoch_labels, hist_dict["val_acc"],'go', label="Validation")
 plt.legend(loc="best")
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
-                          
+
+# evaluating final performance using test data
 print("Checking accuracy on test set...")
 acc = model.evaluate(x_test,y_test,batch_size=512)
 print("\nAccuracy on test set: " + str(acc[1]))
